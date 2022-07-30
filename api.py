7040dea0fe8a6a5bbb236332ac540b5012e85d2d
Fabian Offert <fabian.offert@gmail.com>
2022-07-31 00:09:04 +0100

    Get rid of track modifications warning

--- api.py
+++ api.py
@@ -10,7 +10,8 @@ from io import BytesIO
 import PIL
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/fabian/clip_server/db.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:////home/fabian/clip_server/db.db'
 db = SQLAlchemy(app)
 
 class Embeddings(db.Model): 
